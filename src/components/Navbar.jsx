2e5954dd481bfbe03c21612cffa446a217219cfe
zoena5303 <smile1997590@gmail.com>
2025-09-12 09:36:45 +0800

    9/12導覽列變色/關於我們手機板修改

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "../scss/Navbar.scss";   // SCSS 樣式
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import logo from "../assets/images/AP-LOGO.svg";
 
 // 引入 icons
@@ -17,54 +17,54 @@ const Navbar = () => {
     <nav className="navbar">
       {/* 左側 LOGO：點擊回首頁 */}
       <div className="logo">
-        <Link to="/">
+        <NavLink to="/" end>
           <img src={logo} alt="品牌 Logo" className="logo-img" />
-        </Link>
+        </NavLink>
       </div>
 
       {/* 右側選單 */}
       <ul className="nav-links">
         <li>
-          <Link to="/aboutme">
+          <NavLink to="/aboutme">
             <img src={AboutIcon} alt="關於我們" />
             <span>關於我們</span>
-          </Link>
+          </NavLink>
         </li>
         <li>
-          <Link to="/pricing">
+          <NavLink to="/pricing">
             <img src={PriceIcon} alt="價目表" />
             <span>價目表</span>
-          </Link>
+          </NavLink>
         </li>
         <li>
-          <Link to="/nailtime">
+          <NavLink to="/nailtime">
             <img src={TimeIcon} alt="美甲時光" />
             <span>美甲時光</span>
-          </Link>
+          </NavLink>
         </li>
         <li>
-          <Link to="/storeenvironment">
+          <NavLink to="/storeenvironment">
             <img src={RoomIcon} alt="店面環境" />
             <span>店面環境</span>
-          </Link>
+          </NavLink>
         </li>
         <li>
-          <Link to="/colorexplorer">
+          <NavLink to="/colorexplorer">
             <img src={ColorIcon} alt="指尖更衣室" />
             <span>指尖更衣室</span>
-          </Link>
+          </NavLink>
         </li>
         <li>
-          <Link to="/faq">
+          <NavLink to="/faq">
             <img src={QaIcon} alt="Q&A" />
             <span>Q&A</span>
-          </Link>
+          </NavLink>
         </li>
         <li>
-          <Link to="/blog">
+          <NavLink to="/blog">
             <img src={BlogIcon} alt="指尖保健室" />
             <span>指尖保健室</span>
-          </Link>
+          </NavLink>
         </li>
       </ul>
     </nav>
