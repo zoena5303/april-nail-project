b975fc0ee0d57e1de66d0568d50bf100b78f3ca9
zoena5303 <smile1997590@gmail.com>
2025-09-18 10:39:07 +0800

    修改顏色跟大小'

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,8 @@
+// src/components/Navbar.jsx
 import React, { useState } from "react";
-import "../scss/Navbar.scss";   // SCSS 樣式
+import "../scss/Navbar.scss";
 import { NavLink } from "react-router-dom";
-import { FaBars, FaTimes } from "react-icons/fa"; // 漢堡選單 + 關閉 icon
+import { FaBars, FaTimes } from "react-icons/fa";
 
 import logo from "../assets/images/AP-LOGO.svg";
 
@@ -17,11 +18,13 @@ import BlogIcon from "../assets/images/Blog.svg";
 const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
 
+  const handleCloseMenu = () => setMenuOpen(false);
+
   return (
     <nav className="navbar">
       {/* 左側 LOGO：點擊回首頁 */}
       <div className="logo">
-        <NavLink to="/" end>
+        <NavLink to="/" end onClick={handleCloseMenu}>
           <img src={logo} alt="品牌 Logo" className="logo-img" />
         </NavLink>
       </div>
@@ -29,43 +32,43 @@ const Navbar = () => {
       {/* 右側選單 */}
       <ul className={`nav-links ${menuOpen ? "active" : ""}`}>
         <li>
-          <NavLink to="/aboutme" onClick={() => setMenuOpen(false)}>
+          <NavLink to="/aboutme" onClick={handleCloseMenu}>
             <img src={AboutIcon} alt="關於我們" />
             <span>關於我們</span>
           </NavLink>
         </li>
         <li>
-          <NavLink to="/pricing" onClick={() => setMenuOpen(false)}>
+          <NavLink to="/pricing" onClick={handleCloseMenu}>
             <img src={PriceIcon} alt="價目表" />
             <span>價目表</span>
           </NavLink>
         </li>
         <li>
-          <NavLink to="/nailtime" onClick={() => setMenuOpen(false)}>
+          <NavLink to="/nailtime" onClick={handleCloseMenu}>
             <img src={TimeIcon} alt="美甲時光" />
             <span>美甲時光</span>
           </NavLink>
         </li>
         <li>
-          <NavLink to="/storeenvironment" onClick={() => setMenuOpen(false)}>
+          <NavLink to="/storeenvironment" onClick={handleCloseMenu}>
             <img src={RoomIcon} alt="店面環境" />
             <span>店面環境</span>
           </NavLink>
         </li>
         <li>
-          <NavLink to="/colorexplorer" onClick={() => setMenuOpen(false)}>
+          <NavLink to="/colorexplorer" onClick={handleCloseMenu}>
             <img src={ColorIcon} alt="指尖更衣室" />
             <span>指尖更衣室</span>
           </NavLink>
         </li>
         <li>
-          <NavLink to="/faq" onClick={() => setMenuOpen(false)}>
+          <NavLink to="/faq" onClick={handleCloseMenu}>
             <img src={QaIcon} alt="Q&A" />
             <span>Q&A</span>
           </NavLink>
         </li>
         <li>
-          <NavLink to="/blog" onClick={() => setMenuOpen(false)}>
+          <NavLink to="/blog" onClick={handleCloseMenu}>
             <img src={BlogIcon} alt="指尖保健室" />
             <span>指尖保健室</span>
           </NavLink>
